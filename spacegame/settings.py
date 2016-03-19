8e8dac7c50856e63544da3a3e738132f1e4a9a2a
Allan Berry <allan.berry@gmail.com>
2016-03-19 16:50:23 -0500

    Static files working.

--- spacegame/settings.py
+++ spacegame/settings.py
@@ -112,3 +112,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT =  PROJ_DIR.child('static')
+
+STATICFILES_DIRS = (BASE_DIR.child('static'),)
