e6a511d7662f3ce101c57f54273cc852182ac2a3
Allan Berry <allan.berry@gmail.com>
2016-03-19 16:34:18 -0500

    Hello World operational.

--- spacegame/settings.py
+++ spacegame/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'spacegame.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR.child('templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ spacegame/views.py
@@ -0,0 +1,34 @@
+from django.views.generic.base import TemplateView
+
+class ViewMixin(object):
+    '''
+    Mixin for shared functionality across views.
+    '''
+ 
+    def nav_relative(self, *args, **kwargs):
+        '''
+        A data structure to allow local wayfinding
+        This defines the structure.  Meant to be heavily amended in subclass.
+        '''
+        return [
+            {'name': 'Home',
+             'url': reverse_lazy('home')},
+        ]
+
+    def page_title(self):
+        return 'Spacegame'
+
+    def window_title(self):
+        return self.page_title()
+
+
+class HomeView(ViewMixin, TemplateView):
+    '''
+    The main home page.
+    '''
+    template_name = "spacegame/home.html"
+    def window_title(self):
+        return 'Home'
+
+    def page_title(self):
+        return 'Welcome to Spacegame'
